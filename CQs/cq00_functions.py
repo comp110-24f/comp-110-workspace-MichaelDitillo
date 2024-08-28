0addcae420c17f9f61a47d025ff67db02ee40e4e
MichaelDitillo <ditillom@unc.edu>
2024-08-28 14:21:14 -0400

    Finished Exercise 00!

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""ChallangeQuestion1."""
+
+__author__ = "730660760"
+
+
+def mimic(message: str) -> str:
+    """Mimicing."""
+    return message
+
+
+def main() -> None:
+    """MainFunction."""
+    print(mimic(message=input("What is your message")))
+
+
+if __name__ == "__main__":
+    main()
